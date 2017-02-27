caa41b080229618ae45b94796c2236aad466b607
Kasper Dokter <dokter@cwi.nl>
2017-02-27 15:18:54 +0100

    Added checkers framework.

--- reo-semantics/src/test/java/nl/cwi/reo/interpret/connectors/ConnectorTest.java
+++
@@ -1,5 +0,0 @@
-package nl.cwi.reo.interpret.connectors;
-
-public class ConnectorTest {
-
-}
--- reo-semantics/src/test/java/nl/cwi/reo/interpret/connectors/ReoConnectorTests.java
+++ reo-semantics/src/test/java/nl/cwi/reo/interpret/connectors/ReoConnectorTests.java
@@ -1,13 +1,16 @@
-package nl.cwi.reo.interpret.ports;
+package nl.cwi.reo.interpret.connectors;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import nl.cwi.reo.interpret.ports.Port;
+import nl.cwi.reo.interpret.ports.PortType;
+import nl.cwi.reo.interpret.ports.PrioType;
 import nl.cwi.reo.interpret.typetags.TypeTag;
 
-public class PortTest {
-	
+public class ReoConnectorTests {
+
 	@Test
 	public void join_OriginalHasNoTypes() {
 		Port origin = new Port("origin", PortType.NONE, PrioType.NONE, new TypeTag(""), false);
@@ -19,15 +22,4 @@ public class PortTest {
 		assertEquals(composed.getTypeTag().toString(), "int");
 	}
 	
-	@Test
-	public void join_Inherit() {
-		Port origin = new Port("origin", PortType.IN, PrioType.AMPERSANT, new TypeTag("string"), false);
-		Port target = new Port("target", PortType.OUT, PrioType.NONE, new TypeTag("int"), false);
-		Port composed = target.join(origin);
-		
-		assertEquals(composed.getType(), PortType.IN);
-		assertEquals(composed.getPrioType(), PrioType.NONE);
-		assertEquals(composed.getTypeTag().toString(), "string");
-	}
-
 }
