1dbb6166400414c1b481227541c4a9d3b68cd514
Kasper Dokter <dokter@cwi.nl>
2017-02-27 14:03:33 +0100

    Added unit test for signature expressions.

--- reo-semantics/src/main/java/nl/cwi/reo/util/Monitor.java
+++ reo-semantics/src/main/java/nl/cwi/reo/util/Monitor.java
@@ -12,40 +12,55 @@ public final class Monitor {
 	 * List of all messages.
 	 */
 	private final List<Message> messages;
-	
+
 	/**
 	 * Constructs a new compilation monitor.
 	 */
 	public Monitor() {
 		messages = new ArrayList<Message>();
 	}
-	
+
 	/**
 	 * Add a new message to this monitor.
-	 * @param location		Location 
-	 * @param msg			message
+	 * 
+	 * @param location
+	 *            Location
+	 * @param msg
+	 *            message
 	 */
 	public void add(Location location, String msg) {
 		messages.add(new Message(MessageType.ERROR, location, msg));
 	}
-	
+
 	/**
 	 * Add a new message to this monitor.
-	 * @param msg		message
+	 * 
+	 * @param msg
+	 *            message
 	 */
 	public void add(String msg) {
 		messages.add(new Message(MessageType.ERROR, msg));
 	}
-	
+
+	/**
+	 * Gets the list of messages in the monitor.
+	 * 
+	 * @return list of messages
+	 */
+	public List<Message> getMessages() {
+		return messages;
+	}
+
 	/**
 	 * Print all messages to the standard output.
+	 * 
 	 * @return true if there are some messages printed, and false otherwise.
 	 */
 	public boolean print() {
-		if (messages.isEmpty()) 
+		if (messages.isEmpty())
 			return false;
 		for (Message msg : messages)
 			System.out.println(msg.toString());
-		return true;			
+		return true;
 	}
 }
