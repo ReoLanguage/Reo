0fa017580c22fbbf722aed48c01a2322ed84ce42
Kasper Dokter <dokter@cwi.nl>
2017-03-06 01:13:26 +0100

    Moved examples from Lykos runtime to examples folder.

--- reo-runtime-java-lykos/src/main/java/nl/cwi/pr/runtime/examples/thesis/npb/lu/Relations.java
+++
@@ -1,8 +0,0 @@
-package nl.cwi.pr.runtime.examples.thesis.npb.lu;
-
-public class Relations {
-
-	public static boolean GreaterThan0(Object object) {
-		return object instanceof Integer && ((Integer) object) > 0;
-	}
-}
