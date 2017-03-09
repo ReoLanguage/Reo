1c53d05b73c3425714b18780ea09b48cb65dea25
Kasper Dokter <dokter@cwi.nl>
2017-03-09 17:52:59 +0100

    Started with Java template.

---
+++ reo-compiler/src/main/java/nl/cwi/reo/compile/targetcode/ComponentJava.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package nl.cwi.reo.compile.targetcode;
+
+/**
+ * @author kasper
+ *
+ */
+public final class ComponentJava {
+
+	private final String name;
+	
+	public ComponentJava(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
