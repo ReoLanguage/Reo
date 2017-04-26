7f6972d674cd09964c2bef4a6c48d4db50d3461c
Maarten Smeyers <maartensmeyers@gmail.com>
2017-04-26 13:42:46 +0200

    Testing grouping and ungrouping

---
+++ reo-online-editor/group.js
@@ -0,0 +1,43 @@
+(function() {
+  var canvas = this.__canvas = new fabric.Canvas('c', { selection: false });
+  fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
+  var active, isDown, origX, origY, origLeft, origTop;
+  var mode = 'select';
+  var id = '0';
+
+ var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    width: 300,
+    height: 300,
+    fill: 'transparent',
+    stroke: '#000',
+    strokeWidth: 1,
+    hoverCursor: 'default',
+    originX: 'left',
+    originY: 'top',
+    //hasBorders: false,
+    //hasControls: false,
+  });
+  
+  canvas.add(rect);
+
+  var c = new fabric.Circle({
+    left: 200,
+    top: 200,
+    strokeWidth: 5,
+    radius: 12,
+    fill: '#fff',
+    stroke: '#000',
+    hasControls: false
+  });
+   
+  canvas.add(c);
+  
+  var group = new fabric.Group([ rect.clone(), c.clone() ]);
+  
+  canvas.remove(rect,c);
+  
+  canvas.add(group);
+
+})();
