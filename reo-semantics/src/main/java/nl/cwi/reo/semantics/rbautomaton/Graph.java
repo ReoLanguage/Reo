2eb5160d15a544f9c11005a515220d1fbd3f4ba9
K4r4c <benjamin.lion@sfr.fr>
2017-05-10 14:32:17 +0200

    Hypergraph data structure, composition and hidding; and some examples

--- reo-semantics/src/main/java/nl/cwi/reo/semantics/rbautomaton/Graph.java
+++ reo-semantics/src/main/java/nl/cwi/reo/semantics/rbautomaton/Graph.java
@@ -14,8 +14,12 @@ import nl.cwi.reo.interpret.ports.Port;
 import nl.cwi.reo.semantics.predicates.Conjunction;
 import nl.cwi.reo.semantics.predicates.Formula;
 
+@Deprecated
 public class Graph {
 
+	List<Set<GraphNode>> listLeaves = new ArrayList<Set<GraphNode>>();
+	List<Set<GraphNode>> listNodes = new ArrayList<Set<GraphNode>>();
+	int connectedParts = 0;
 	private List<GraphNode> n;
 
 	public Graph(Set<Rule> l) {
@@ -33,6 +37,9 @@ public class Graph {
 					k.addChilds(Arrays.asList(r));
 					r.addParents(Arrays.asList(k));
 				}
+				if (i != j && k.hasExclusiveEdge(r)) {
+					k.addExcNodes(Arrays.asList(r));
+				}
 			}
 		}
 		this.n = list;
@@ -44,6 +51,45 @@ public class Graph {
 
 	public List<GraphNode> getNodes() {
 		return n;
+	}
+	
+	public Graph removeNode(GraphNode node){
+		if(n.contains(node)){
+			for(GraphNode child : n.get(n.indexOf(node)).getChilds()){
+				child.removeChilds(Arrays.asList(node));
+				child.removeParents(Arrays.asList(node));
+			}
+			n.remove(node);
+		}
+		return this;
+	}
+	
+	public Graph mergeNode(GraphNode n1, GraphNode n2){
+		
+		removeNode(n1);
+		removeNode(n2);
+		
+		Set<GraphNode> childs = new HashSet<>(n1.getChilds());
+		childs.addAll(n2.getChilds());
+		childs.remove(n2);
+		childs.remove(n1);
+		
+		Set<GraphNode> parents = childs;
+		
+		
+		GraphNode n3 = new GraphNode(merge(Arrays.asList(n1.getRule(),n2.getRule())),new ArrayList<>(childs),new ArrayList<>(parents));
+		n3.addExcNodes(n1.getExcNodes());
+		n3.addExcNodes(n2.getExcNodes());
+		for(GraphNode child : n3.getChilds()){
+			if(!n.get(n.indexOf(child)).getParents().contains(n3))
+				n.get(n.indexOf(child)).addParents(Arrays.asList(n3));
+		}
+		for(GraphNode parent : n3.getParents()){
+			if(!n.get(n.indexOf(parent)).getChilds().contains(n3))
+				n.get(n.indexOf(parent)).addChilds(Arrays.asList(n3));
+		}
+		n.add(n3);
+		return this;
 	}
 
 	public void resetFlags() {
@@ -121,12 +167,10 @@ public class Graph {
 	}
 	
 	public Graph isolate1(){
-		List<GraphNode> newNodes = new ArrayList<GraphNode>();
-		
-		Set<Set<Rule>> newRuleSet = new HashSet<>();
-		int connectedParts = 0;
-		List<Set<GraphNode>> listLeaves = new ArrayList<Set<GraphNode>>();
+
+
 		listLeaves.add(new HashSet<GraphNode>());
+		listNodes.add(new HashSet<>());
 		int j = 0;
 		
 		//Go over all nodes in the graph, find connected parts and all leaf nodes.
@@ -157,9 +201,11 @@ public class Graph {
 					}
 				}
 				
+				listNodes.get(j).add(child);
 				//It's a leaf, add it to the listLeave
 				if(i==1){
 					listLeaves.get(j).add(child);
+					listNodes.get(j).remove(child);
 				}
 				for (GraphNode n : child.getParents())
 					if (!n.isVisited() && !queue.contains(n))
@@ -168,14 +214,21 @@ public class Graph {
 			}
 			if(connectedParts != 0){
 				listLeaves.add(new HashSet<GraphNode>());
+				listNodes.add(new HashSet<GraphNode>());
 				j++;
 			}
 			connectedParts=0;
 		}
 		
-		resetFlags();
-		newRuleSet = new HashSet<>();
+		return isolate3();
+	}
+	
+	public Graph isolate3(){
+		List<GraphNode> newNodes = new ArrayList<GraphNode>();
 		
+		Set<Set<Rule>> newRuleSet = new HashSet<>();
+		
+		resetFlags();
 		//Go through all leaves, and build the new list of rules
 		for(Set<GraphNode> setNode : listLeaves){
 			for(GraphNode node : setNode){
@@ -195,7 +248,6 @@ public class Graph {
 				
 				//A rule is added to the list if the node is a child, and if its rule compose with the new composed rule
 				while (!queue.isEmpty() || (child != null && !child.isVisited())) {
-					connectedParts++;
 					child.setFlag(true);
 					for (GraphNode n : child.getChilds()){
 						if (!n.isVisited() && !queue.contains(n) && n.getRule().canSync(merge(new ArrayList<>(newRule)))){
@@ -216,8 +268,73 @@ public class Graph {
 		return new Graph(newNodes);
 
 	}
-	
 
+//	for(Set<GraphNode> setNode : listLeaves){
+//		for(GraphNode node : setNode){
+//			Set<Rule> newRule = new HashSet<>();
+//			
+//			Queue<GraphNode> queue = new LinkedList<GraphNode>();
+//			GraphNode child = null;
+//			if(!(node.isVisited())){
+//				queue.addAll(node.getChilds());
+//				child = queue.poll();
+//				newRule.add(node.getRule());
+//			}
+//			
+//			node.setFlag(true);
+//			if(child != null)
+//				newRule.add(child.getRule());
+//			
+//			//A rule is added to the list if the node is a child, and if its rule compose with the new composed rule
+//			while (!queue.isEmpty() || (child != null && !child.isVisited())) {
+//				child.setFlag(true);
+//				for (GraphNode n : child.getChilds()){
+//					if (!n.isVisited() && !queue.contains(n) && n.getRule().canSync(merge(new ArrayList<>(newRule)))){
+//						newRule.add(n.getRule());
+//						queue.add(n);
+//					}
+//				}
+//				child = queue.poll();
+//			}
+//			resetFlags();
+//			newRuleSet.add(newRule);
+//		}
+//	}
+//	
+//	for (Set<Rule> newRule : newRuleSet)
+//		newNodes.add(new GraphNode(merge(new ArrayList<>(newRule)), new ArrayList<>(), new ArrayList<>()));
+//
+//	return new Graph(newNodes);
+	
+	public Graph mergeNodes(){
+
+		Queue<GraphNode> queue = new LinkedList<GraphNode>();
+		queue.addAll(getNodes());
+		
+		while (!queue.isEmpty()){
+			GraphNode n = queue.poll();
+			if(n.getExcNodes().size()!=0)
+				continue;
+			else{
+				for (GraphNode c : n.getChilds()){
+					if (c.getExcNodes().size()==0){
+						mergeNode(n,c);
+						queue.clear();
+						queue.addAll(getNodes());
+						break;
+					}
+					else if(n.getChilds().size()==1 && n.getChilds().get(0).getExcNodes().containsAll(c.getExcNodes()) && c.getExcNodes().containsAll(n.getChilds().get(0).getExcNodes())){
+						mergeNode(n,c);
+						queue.clear();
+						queue.addAll(getNodes());
+						break;
+					}
+				}
+			}
+		}
+		return this;
+	}
+	
 	public Rule merge(List<Rule> list) {
 		List<Formula> clauses = new ArrayList<Formula>();
 		Map<Port, Boolean> sync = new HashMap<>();
