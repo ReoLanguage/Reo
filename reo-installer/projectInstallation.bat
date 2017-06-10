a5390e0d32d48d7dc4dd1ab7ff2e2a7d509e2ced
K4r4c <benjamin.lion@sfr.fr>
2017-06-10 17:56:21 +0200

    reo-installer updates

--- reo-installer/projectInstallation.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title This is your first batch script!
-echo Welcome to batch scripting!
-pause
--- reo-installer/src/main/resources/installer.sh
+++ reo-installer/src/main/resources/installer.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
-
   BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "
+#!/bin/bash
+
+alias reo=\"java -jar $BASEDIR/bin/reo-1.0.jar\"
+
+export CLASSPATH=\"$CLASSPATH:.:$BASEDIR/bin/reo-runtime-java.jar:$BASEDIR/bin/reo-runtime-java-lykos.jar\"
+" >> $BASEDIR/initreo.sh
+
+echo "
+===================================
+=                                 =
+=   README for Reo installation   =
+=                                 =
+===================================
 
 
-if grep -q "alias reo="  ~/.bashrc
-then 
-   echo "Alias already exist";
-else
-  echo "alias reo='java -jar $BASEDIR/bin/reo-1.0.jar'" >> ~/.bashrc 
-fi
+=================
+Unix-like Systems and Mac :
+=================
 
-if grep -q ".reosource"  ~/.bashrc
-then
-   echo "ClassPath already set";
-else
-  echo ". $BASEDIR/.reosource" >> ~/.bashrc
-fi
+You need to set some environment variable before being able to use Reo. 
+To configure reo for your current shell, execute the following commands:
+
+        >$ export CLASSPATH=\"\$CLASSPATH:.:$BASEDIR/bin/reo-runtime-java.jar:$BASEDIR/bin/reo-runtime-java-lykos.jar\"
+
+        >$ alias reo=\"java -jar $BASEDIR/bin/reo-1.0.jar\"
+
+If you want to make Reo setting automatic for each Shell sessions, happen in your ~/.bashrc the following command :
+
+        >$ source $BASEDIR/initreo.sh
+
+Close and open a new terminal to make it effective or run
+
+        >$ source ~/.bashrc
+" >> $BASEDIR/README
+
