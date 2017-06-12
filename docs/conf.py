b75a6a5ee8d153cc07af4367bfde0a8a04894221
Kasper Dokter <kasperdokter@gmail.com>
2017-06-12 17:37:26 +0200

    Updated Reo compiler

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Reo'
-copyright = u'2016, Kasper Dokter'
+copyright = u'2016-2017, Kasper Dokter'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
