919f45f2318e1b338ff97cdbb47ef7d012045294
Kasper Dokter <kasperdokter@gmail.com>
2017-07-12 11:41:19 +0200

    clean up

---
+++ reo-runtime-java/src/main/java/nl/cwi/reo/runtime/SchedulablePort.java
@@ -0,0 +1,82 @@
+package nl.cwi.reo.runtime;
+
+@SuppressWarnings("initialization")
+public class SchedulablePort<T> implements Port<T> {
+	private Component prod;
+	private Component cons;
+	private volatile T put;
+	private volatile boolean get;
+	public void setProducer(Component p) { prod = p; }
+	public void setConsumer(Component c) { cons = c; }	
+	public void setGet() { get = true; }
+	public void setPut(T datum) { put = datum; }
+	public boolean hasGet() { return get; }
+	public T hasPut() { return put; }
+	public T peek() { return put; }
+	
+	public T take() { 
+		T datum;
+		while ((datum = put) == null) { }
+		put = null; 
+		get = false; 
+		prod.activate();
+		return datum; 
+	}
+	public void activateProducer() { prod.activate(); }
+	public void activateConsumer() { cons.activate(); }
+	public void put(T datum) {
+		if (datum == null) throw new NullPointerException();
+		// Signal the scheduler
+		while (put != null) { }
+		put = datum; 
+		cons.activate();
+		// Wait for signal from scheduler
+		while (!get && put != null) { }
+	}
+	public T get() {
+		// Signal the scheduler
+		get = true; 
+		prod.activate();
+		// Wait for signal from scheduler
+		T datum;
+		while ((datum = put) == null) { }
+		put = null; 
+		get = false; 
+		prod.activate();
+		return datum;
+	}
+	public boolean put(T datum, long timeout) {
+		if (datum == null) throw new NullPointerException();
+		// Signal the scheduler
+		long deadline = System.nanoTime() + timeout;
+		while (put != null) { }
+		put = datum; 
+		cons.activate();
+		// Wait for signal from scheduler
+		while (!get && put != null) { 
+			if (System.nanoTime() >= deadline) {
+				put = null;  // use compare and set here
+				return false; // not thread safe, since datum may have been taken
+			}
+		}
+		return true;
+	}
+	public T get(long timeout) {
+		// Signal the scheduler
+		long deadline = System.nanoTime() + timeout;
+		get = true; 
+		prod.activate();
+		// Wait for signal from scheduler
+		T datum;
+		while ((datum = put) == null) {
+			if (System.nanoTime() >= deadline) {
+				put = null;  // use compare and set here
+				return null; // not thread safe, since datum may have been taken
+			}
+		}
+		put = null; 
+		get = false; 
+		prod.activate();
+		return datum;
+	}
+}
---
+++ reo-runtime-java/src/main/java/nl/cwi/reo/runtime/Scheduler.java
@@ -0,0 +1,9 @@
+package nl.cwi.reo.runtime;
+
+public interface Scheduler extends Runnable {
+	
+	/**
+	 * Activates this component.
+	 */
+	public void activate();
+}
