1f31679794cd9eaf691320785ad7bbd1f115f46d
k4r4c <benjamin.lion@sfr.fr>
2019-10-22 15:59:16 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -132,7 +132,11 @@ html_theme_path = ['_themes', ]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static', ]
+html_static_path = ['_static']
 
+def setup(app):
+    app.add_stylesheet("css/theme.css")
+    
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
